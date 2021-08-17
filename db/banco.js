5456716d6c096a088b0d4dfa6f40660d9d4f1da7
lfdinisfernandes <lfdinisfernandes@gmail.com>
2021-08-16 23:57:06 -0300

    mudando os projetos

---
+++ db/banco.js
@@ -0,0 +1,11 @@
+//conexão com obando de dados MySQL
+/*
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : 'localhost:3306',
+    user     : 'root',
+    password : 'Mustang8v081216',
+    database : 'calke'
+  });
+*/
